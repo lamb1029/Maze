a1d209559f05a505f9a669e0558093c213411668
lamb1029 <90374849+lamb1029@users.noreply.github.com>
2021-12-01 19:38:44 +0900

    Generate Maze Algorithm

--- Board.cs
+++ Board.cs
@@ -18,17 +18,109 @@ namespace maze
 
         public void Initialize(int size)
         {
+            if (size % 2 == 0)
+                return;
+
             _tile = new TileType[size, size];
             _size = size;
 
-            for(int y = 0; y < _size; y++)
+            //mazes for Programmers
+            //GenerateByBinaryTree();
+            GenerateBySideWinder();
+        }
+
+        private void GenerateByBinaryTree()
+        {
+            //Binary Tree Algorithm
+            //길 막는 작업
+            for (int y = 0; y < _size; y++)
             {
-                for(int x = 0; x < _size; x++)
+                for (int x = 0; x < _size; x++)
                 {
-                    if (x == 0 || x == _size - 1 || y == 0 || y == _size - 1)
+                    //if (x == 0 || x == _size - 1 || y == 0 || y == _size - 1)
+                    if (x % 2 == 0 || y % 2 == 0)
                         _tile[y, x] = TileType.Wall;
                     else
                         _tile[y, x] = TileType.Empty;
+                }
+            }
+            //랜덤으로 우측이나 아래로 길 뚫기
+            Random rand = new Random();
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+                    if (y == _size - 2 && x == _size - 2)
+                        continue;
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                    }
+                    else
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                    }
+                }
+            }
+        }
+
+        private void GenerateBySideWinder()
+        {
+            //길 막는 작업
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    //if (x == 0 || x == _size - 1 || y == 0 || y == _size - 1)
+                    if (x % 2 == 0 || y % 2 == 0)
+                        _tile[y, x] = TileType.Wall;
+                    else
+                        _tile[y, x] = TileType.Empty;
+                }
+            }
+            Random rand = new Random();
+            for (int y = 0; y < _size; y++)
+            {
+                int count = 1;
+                for (int x = 0; x < _size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+                    if (y == _size - 2 && x == _size - 2)
+                        continue;
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        count++;
+                    }
+                    else
+                    {
+                        int randomIndex = rand.Next(0, count);
+                        _tile[y + 1, x - randomIndex * 2] = TileType.Empty;
+                        count = 1;
+                    }
                 }
             }
         }
